6e920388b59bbcbb9a5a74d7d98e25b42bc72024
kinichen <kini.chen.mail@gmail.com>
2025-03-15 17:10:24 -0400

    Added dimension reduced kinase amino acid embeddings

---
+++ kinase_AA_dim_reduction.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+
+kinase_embedd = np.load("global_embeddings_max_length.npy")
+# print(kinase_embedd.shape) # Original shape ~ (442, 15599)
+
+# Compute full svd for comparison
+U, S, Vt = np.linalg.svd(kinase_embedd)
+
+cumulative_variance = np.cumsum(S**2) / np.sum(S**2) # cumulative up to each k
+
+# Choose k for 90% variance retention
+optimal_k = np.argmax(cumulative_variance >= 0.95) + 1
+print(f"Optimal k for 95% variance retention: {optimal_k}")
+
+# So let's choose projecting onto a 38-dimensional space
+
+# If wanted to check explained variance for a specific k
+def explained_variance(k):
+    explained_variance = cumulative_variance[k-1]
+    print(f"Explained Variance for k={k}: {explained_variance * 100:.2f}%")
+
+
+
+
